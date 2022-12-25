53d3a7728706e8bc9b68204badf83ee263e4919c
sidbootstrap <siddharhb@keemail.me>
2022-12-26 03:46:49 +0530

    Main function.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-import java.io.File;
 import java.io.IOException;
-import java.util.Map;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception{
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException{
 
         WorkingDirectory wd = new WorkingDirectory();
-        System.out.println(wd.Checksum());
+        BaseLine bl = new BaseLine();
+        System.out.print("Folder Path : ");
+        String path = sc.nextLine();
+        System.out.println("Choose one of the options : ");
+        System.out.println("1.Create a base file");
+        System.out.println("2.Check File Integrity");
+        System.out.print("Your Choice : ");
+        int choice = sc.nextInt();
+        if(choice == 1){System.out.println("Creating Base File...");bl.BaseFileOrigin(path);System.out.println("Base file Created.");}
+        else if(choice == 2){System.out.println("Checking File Integrity...");wd.CheckAlerts(path);}
+        else{System.out.println("Enter a valid option.");}
 
     }
 }
