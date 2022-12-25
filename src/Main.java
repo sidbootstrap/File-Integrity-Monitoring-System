0d4e784c5986861ef7239acf2c2ead9fc646446a
sidbootstrap <siddharhb@keemail.me>
2022-12-26 04:44:40 +0530

    0

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,31 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) throws NoSuchAlgorithmException, IOException{
-
-        WorkingDirectory wd = new WorkingDirectory();
-        BaseLine bl = new BaseLine();
-        System.out.print("Folder Path : ");
-        String path = sc.nextLine();
-        System.out.println("Choose one of the options : ");
-        System.out.println("1.Create a base file");
-        System.out.println("2.Check File Integrity");
-        System.out.print("Your Choice : ");
-        int choice = sc.nextInt();
-        if(choice == 1){System.out.println("Creating Base File...");bl.BaseFileOrigin(path);System.out.println("Base file Created.");}
-        else if(choice == 2){System.out.println("Checking File Integrity...");wd.CheckAlerts(path);}
-        else{System.out.println("Enter a valid option.");}
-
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        while (true) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                WorkingDirectory wd = new WorkingDirectory();
+                BaseLine bl = new BaseLine();
+                System.out.print("Folder Path : ");
+                String path = sc.nextLine();
+                System.out.println("\nChoose one of the options : ");
+                System.out.println("1.Create a base file.");
+                System.out.println("2.Check File Integrity.");
+                System.out.print("\nEnter Your Choice -> ");
+                int choice = sc.nextInt();
+                if (choice == 1) {
+                    System.out.println("\nCreating Base File...");
+                    bl.BaseFileOrigin(path);
+                    System.out.println("\nBase file Created.");
+                } else if (choice == 2) {
+                    System.out.println("\nChecking File Integrity...");
+                    wd.CheckAlerts(path);
+                } else {
+                    System.out.println("\nEnter a valid option.\n");
+                }
+            } catch (Exception e) {
+            }
+        }
     }
 }
